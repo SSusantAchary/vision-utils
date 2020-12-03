e4f9e21646087a3ec113023cee7e03d663224ab4
Susant Achary <acharysusant@gmail.com>
2020-12-03 14:50:53 +0530

    vision utils package

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='vision-utils',
+    version='0.1.0',
+    description='A Vision Utility Python package',
+    url='https://github.com/ssusantachary/vision-utils',
+    author='Susant Achary',
+    author_email='acharysusant@gmail.com',
+    license='MIT',
+    packages=['vision-utils'],
+    install_requires=['cv2>=3.4',
+                      'numpy',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
---
+++ vision-utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+vision-utils.
+
+An example python library.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Susant Achary'
+__credits__ = 'To the Curious Soul'
